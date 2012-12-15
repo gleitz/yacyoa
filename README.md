7838dcf8b66a91f6a4643b252fc70874b8482935
Benjamin Gleitzman <gleitz@mit.edu>
2012-12-14 21:24:42 -0800

    updated readme

--- README.md
+++ README.md
@@ -4,11 +4,18 @@ Yet Another Choose Your Own Adventure
 Your digital host guides you through a deathly Choose Your Own Adventure game pirated from the Library of Congress.
 
 
-Usage:
+Usage
+-----
 
-*   `python generate.py`
-*   `generate.navigate_story()`
+*  `python generate.py`
+*  `generate.navigate_story()`
 
-Extra notes:
+Author
+------
 
-*   Can be used with `telephone.ino` to upload the game to an Arduino
+*  Benjamin Gleitzman [@gleitz](http://twitter.com/gleitz)
+
+Extra notes
+-----------
+
+*  Can be used with `telephone.ino` to upload the game to an Arduino
--- telephone.ino
+++ telephone.ino
@@ -8,7 +8,6 @@
 //and the MP3 Shield Library
 #include <SFEMP3Shield.h>
 
-//create and name the library object
 SFEMP3Shield MP3player;
 
 byte result;
@@ -21,9 +20,6 @@ int in = 12;
 int lastState = LOW;
 int trueState = LOW;
 long lastStateChangeTime = 0;
-
-// constants
-
 int stoppedDialingMs = 3000;
 int dialHasFinishedRotatingAfterMs = 100;
 int debounceDelay = 10;
@@ -42,23 +38,19 @@ void loop()
 
 
     int reading = digitalRead(in);
-    //Serial.println(reading);
 
     if ((millis() - lastStateChangeTime) > stoppedDialingMs) {
-      if (needToPrintAll) {
-        Serial.println("total number is " + String(totalNumber));
-        MP3player.stopTrack();
-        MP3player.playTrack(totalNumber);
-        totalNumber = 0;
-        needToPrintAll = 0;
-      }
+        if (needToPrintAll) {
+            Serial.println("total number is " + String(totalNumber));
+            MP3player.stopTrack();
+            MP3player.playTrack(totalNumber);
+            totalNumber = 0;
+            needToPrintAll = 0;
+        }
     }
 
     if ((millis() - lastStateChangeTime) > dialHasFinishedRotatingAfterMs) {
-        // the dial isn't being dialed, or has just finished being dialed.
         if (needToPrint) {
-            // if it's only just finished being dialed, we need to send the number down the serial
-            // line and reset the count. We mod the count by 10 because '0' will send 10 pulses.
             Serial.println(count % 10);
             totalNumber = 10 * totalNumber + (count % 10);
             Serial.println(totalNumber);
@@ -72,14 +64,11 @@ void loop()
         lastStateChangeTime = millis();
     }
     if ((millis() - lastStateChangeTime) > debounceDelay) {
-        // debounce - this happens once it's stablized
         if (reading != trueState) {
-            // this means that the switch has either just gone from closed->open or vice versa.
             trueState = reading;
             if (trueState == HIGH) {
-                // increment the count of pulses if it's gone high.
                 count++;
-                needToPrint = 1; // we'll need to print this number (once the dial has finished rotating)
+                needToPrint = 1;
             }
         }
     }
